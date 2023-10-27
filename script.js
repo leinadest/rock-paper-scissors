5a72b067416852c11b35de3b2252dfe12084f077
Daniel Tse <daniel.tse1211@gmail.com>
2023-10-27 16:54:39 -0400

    Modify HTML and add CSS to display and run the game on the webpage
    The game is now much more user friendly, because unlike before,
    the user interface takes place on the browser's webpage

--- script.js
+++ script.js
@@ -1,56 +1,65 @@
+let playerScore = document.querySelector('.playerScore');
+let computerScore = document.querySelector('.computerScore');
+let resultDisplay = document.querySelector('.result');
+const btns = document.querySelectorAll('button');
+
 function getComputerChoice() {
-    return ["Rock", "Paper", "Scissors"][parseInt(Math.random()*3)];
+    return ["rock", "paper", "scissors"][parseInt(Math.random()*3)];
 }
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
+function showWinner() {
+    if (playerScore.innerHTML > computerScore.innerHTML) {
+        resultDisplay.innerHTML = "You are the winner!";
+    }
+    if (playerScore.innerHTML < computerScore.innerHTML) {
+        resultDisplay.innerHTML = "The winner is the computer!";
+    }
+}
+
+function endGame() {
+    btns.forEach(btn => btn.removeEventListener('click', playRound));
+}
+
+function playRound(event) {
+    event.stopPropagation();
+    let playerSelection = event.target.classList.item(0);
+    let computerSelection = getComputerChoice();
+
     if (playerSelection == "rock" && computerSelection == "paper") {
-        return "You Lose! Paper beats Rock"
+        resultDisplay.innerHTML = "You Lose! Paper beats Rock";
+        computerScore.innerHTML = Number(computerScore.innerHTML) + 1;
     }
     if (playerSelection == "rock" && computerSelection == "scissors") {
-        return "You Win! Rock beats Scissors"
+        resultDisplay.innerHTML = "You Win! Rock beats Scissors";
+        playerScore.innerHTML = Number(playerScore.innerHTML) + 1;
     }
     if (playerSelection == "paper" && computerSelection == "rock") {
-        return "You Win! Paper beats Rock"
+        resultDisplay.innerHTML = "You Win! Paper beats Rock";
+        playerScore.innerHTML = Number(playerScore.innerHTML) + 1;
     }
     if (playerSelection == "paper" && computerSelection == "scissors") {
-        return "You Lose! Scissors beats Paper"
+        resultDisplay.innerHTML = "You Lose! Scissors beats Paper";
+        computerScore.innerHTML = Number(computerScore.innerHTML) + 1;
     }
     if (playerSelection == "scissors" && computerSelection == "rock") {
-        return "You Lose! Rock beats Scissors"
+        resultDisplay.innerHTML = "You Lose! Rock beats Scissors";
+        computerScore.innerHTML = Number(computerScore.innerHTML) + 1;
     }
     if (playerSelection == "scissors" && computerSelection == "paper") {
-        return "You Win! Scissors beats Paper"
+        resultDisplay.innerHTML = "You Win! Scissors beats Paper";
+        playerScore.innerHTML = Number(playerScore.innerHTML) + 1;
     }
     if (playerSelection == computerSelection) {
-        return "It's a draw!"
+        resultDisplay.innerHTML = "It's a draw!";
+    }
+
+    let round = document.querySelector('.round');
+    round.innerHTML = Number(round.innerHTML) + 1;
+    
+    if (playerScore.innerHTML >= 5 || computerScore.innerHTML >= 5) {
+        showWinner();
+        endGame();
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Choose your move (Rock, Paper, or Scissors)")
-        let result = playRound(playerSelection, getComputerChoice());
-        console.log(result);
-        if (result.slice(0, 7) == "You Win") {
-            playerScore++;
-        }
-        if (result.slice(0, 8) == "You Lose") {
-            computerScore++;
-        }
-    }
-    if (playerScore > computerScore) {
-        console.log("You are the winner!")
-    }
-    if (playerScore < computerScore) {
-        console.log("The winner is the computer!")
-    }
-    if (playerScore == computerScore) {
-        console.log("There is no winner; it's a draw!")
-    }
-}
-
-game();
+btns.forEach(btn => btn.addEventListener('click', playRound));
